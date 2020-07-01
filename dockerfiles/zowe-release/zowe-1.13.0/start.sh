e28adeb4d9c5005cd16cccce1371ba7d3e957369
Timothy Gerstel <tgerstel@rocketsoftware.com>
2020-07-01 18:02:06 -0400

    Update tag to proper version
    Signed-off-by: Timothy Gerstel <tgerstel@rocketsoftware.com>

--- dockerfiles/zowe-release/zowe-1.13.0/start.sh
+++ dockerfiles/zowe-release/zowe-1.13.0/start.sh
@@ -10,15 +10,14 @@
 #cp -r vlcvi01 /mnt/c/temp/vlcvi01
 docker run -it \
     -p 9654:9654 \
-    -p 9644:9644 \
+    -p 9544:9544 \
     -p 9653:9653 \
     -h localhost \
 	--add-host=zowe.host.com:127.0.0.1 \
-    --env ZOWE_ZOSMF_HOST=zosmf.host.com \
+    --env ZOWE_ZOSMF_HOST=rs28.rocketsoftware.com \
     --env ZOWE_ZOSMF_PORT=11443 \
-    --env ZOWE_ZSS_HOST=zss.host.com \
-    --env ZOWE_ZSS_PORT=11111 \
-    --env LAUNCH_COMPONENT_GROUPS=DESKTOP,GATEWAY \
+    --env ZWED_agent_http_port=32002 \
+    --env LAUNCH_COMPONENT_GROUPS=DESKTOP \
     zowe/docker:latest $@
 
     
